e76edbd54cf2ced166f6283c2e181013016aa8db
HamzaRazaQureshi <hamzarq999@gmail.com>
2024-02-14 17:55:12 +0500

    Initial structure established

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageGenerator from './Components/ImageGenerator/imageGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageGenerator/>
+    </div>
+  );
+}
+
+export default App;
